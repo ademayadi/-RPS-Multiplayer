fd48119c85b57899b46d85d43e38e869b377b9a5
ademayadi <ademayadi2025@gmail.com>
2026-06-21 13:32:49 +0100

    Compatibilite Render

--- server.js
+++ server.js
@@ -99,6 +99,8 @@ function getResult(a, b) {
     return "p2";
 }
 
-server.listen(3000, () => {
-    console.log("🚀 Serveur lancé http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`🚀 Serveur lancé sur le port ${PORT}`);
 });
